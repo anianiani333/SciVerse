ce4779e0b9a51a0ca013a29e579a4f5dd69c523f
Nino-kharazishvili <87334221+Nino-kharazishvili@users.noreply.github.com>
2025-07-17 16:21:40 +0400

    Update render.yaml and fix wsgi.py for Render deployment

--- wsgi.py
+++ wsgi.py
@@ -2,6 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 from SciVerse import app
+print("Using Python version:", sys.version)
 
 if __name__ == "__main__":
     app.run()
